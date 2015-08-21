3bcda51a6feca453b304ce5dd670c5d6649410fa
qbodart <bodqu@LT7SLSL01.cargolux.com>
2015-08-21 11:51:13 +0200

    commenting

--- src/core/ControlPanel.java
+++ src/core/ControlPanel.java
@@ -31,6 +31,7 @@ public class ControlPanel extends JPanel
 			public void actionPerformed(ActionEvent arg0) 
 			{
 				Agent a = new Agent(30);
+				//this is another comment
 				a.setX(Network.getInstance().getWidth() / 2);
 				a.setY(Network.getInstance().getHeight() / 2);
 				Network.getInstance().addAgent(a);
